d22b304a15c808282fd83838775a917f3518b1cc
hueytvoanh <toan.daothanh@gmail.com>
2023-03-14 20:52:55 +0700

    Update Esp32Dht22Mqtt.ino
    update Soc index, mqtt server

--- Esp32Dht22Mqtt.ino
+++ Esp32Dht22Mqtt.ino
@@ -27,7 +27,8 @@
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 DHT dht(DHTPIN, DHTTYPE);
 
-
+volatile float tempDht, humDht;
+//volatile char tempString[4];
 
 TaskHandle_t checkTempHumTask;
 TaskHandle_t mqttKeepAliveTask;
@@ -39,13 +40,16 @@ const char* password = "0857270632";
 // Update these with values suitable for your network.
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 IPAddress ip(172, 16, 0, 100);
-IPAddress server(192, 168, 1, 7);
+//IPAddress server(192, 168, 1, 7);
+#define MQTT_SERVER "sunnyiot.duckdns.org"
 
 // Callback function header
 void callback(char* topic, byte* payload, unsigned int length);
 
 WiFiClient WifiClient;
-PubSubClient client(server, 1883, callback, WifiClient);
+//PubSubClient client(server, 1883, callback, WifiClient);
+//PubSubClient client(MQTT_SERVER, 1883, callback, WifiClient);
+PubSubClient client(MQTT_SERVER, 1883, WifiClient);
 
 // Callback function
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -103,51 +107,58 @@ void setup()
   dht.begin();
   delay(2000);
 
+
   xTaskCreatePinnedToCore(
      checkTempHumTaskFunction,
      "checkTempHumTaskFunction",
      10000,
      NULL,
-     1,
+     0,
      &checkTempHumTask,
-     1);
+     0);
 
   xTaskCreatePinnedToCore(
      mqttKeepAliveTaskFunction,
      "mqttKeepAliveTaskFunction",
      10000,
      NULL,
-     1,
+     0,
      &mqttKeepAliveTask,
-     1);
+     0);
 
   xTaskCreatePinnedToCore(
      mqttUploadTempHumTaskFunction,
      "mqttUploadTempHumTaskFunction",
      10000,
      NULL,
-     1,
+     0,
      &mqttUploadTempHumTask,
-     1);     
- 
+     0);     
 }
 
 
 void mqttUploadTempHumTaskFunction( void * pvParameters ){
-  
+  float a = 1.1;
+  float temperature = 4.5;
+  char tempString[8];
+  //dtostrf(tempDht, 1, 2, tempString);
   while(1){
+      dtostrf(tempDht, 1, 2, tempString);
+      
       if (client.connect("arduinoClient")) {
-          Serial.print("Connected to broker ");
+          Serial.print("Connected to broker. Begin Publish message ");
           //client.publish("outTopic","hello world");
           //client.subscribe("inTopic");
-          client.publish("sensors","hello world");
-          client.subscribe("sensors");
+          client.publish("sensors",tempString);
+          //client.subscribe("sensors");
+          
+          delay(30000);
+          delay(30000);
       }
       else{
           Serial.print("Can not connected to broker ");
+          delay(2000);
      }
-
-     delay(30000);
   }
 }
 
@@ -163,44 +174,47 @@ void mqttKeepAliveTaskFunction( void * pvParameters ){
 
 
 void checkTempHumTaskFunction( void * pvParameters ){
-  
+  float h, t,f, hif, hic ;
   while(1){
-      delay(5000);
-      float h = dht.readHumidity(); 
-      float t = dht.readTemperature();  
-      float f = dht.readTemperature(true);
+      
+      //dht.begin();   
+      h = dht.readHumidity(); 
+      t = dht.readTemperature();  
+      f = dht.readTemperature(true);
    
       if (isnan(h) || isnan(t) || isnan(f)) {
           Serial.println(F("Failed to read from DHT sensor!"));
-          //return;
+          delay(2000);
       }
-
-      float hif = dht.computeHeatIndex(f, h);
-      float hic = dht.computeHeatIndex(t, h, false);
-
-      Serial.print(F("Humidity: "));
-      Serial.print(h);
-      Serial.print(F("%  Temperature: "));
-      Serial.print(t);
-      Serial.print(F("°C "));
-      Serial.print(f);
-      Serial.print(F("°F  Heat index: "));
-      Serial.print(hic);
-      Serial.print(F("°C "));
-      Serial.print(hif);
-      Serial.println(F("°F"));
+      else{
+          tempDht = t;
+          humDht = h;          
+          hif = dht.computeHeatIndex(f, h);
+          hic = dht.computeHeatIndex(t, h, false);
+          Serial.print(F("Humidity: "));
+          Serial.print(h);
+          Serial.print(F("%  Temperature: "));
+          Serial.print(t);
+          Serial.print(F("°C "));
+          Serial.print(f);
+          Serial.print(F("°F  Heat index: "));
+          Serial.print(hic);
+          Serial.print(F("°C "));
+          Serial.print(hif);
+          Serial.println(F("°F"));
+          delay(30000);
+          delay(30000);
+      }
   }
 }
-
-
 
 void loop()
 {
   //client.loop();
 
-
-  //delay(2000);
 /*
+  delay(20000);
+
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
@@ -231,5 +245,5 @@ void loop()
   Serial.print(F("°C "));
   Serial.print(hif);
   Serial.println(F("°F"));
-  */
+ */ 
 }
